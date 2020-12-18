1839ed9716b40312cf666820f8cbf001c6baa852
phl3bas <phl3bas87@gmail.com>
2020-12-18 16:12:50 +0000

    temp update example for shadows

--- example/index.js
+++ example/index.js
@@ -18,3 +18,26 @@ eases.addEventListener("change", (e) => {
     box.classList.add(e.target.options[e.target.options.selectedIndex].value);
   });
 });
+
+radius.addEventListener("change", (e) => {
+  boxes.forEach((box) => {
+    ["r-1", "r-2", "r-3", "r-4", "r-5", "r-full"].forEach((i) =>
+      box.classList.remove(i)
+    );
+    box.classList.add(
+      `r-${e.target.options[e.target.options.selectedIndex].value}`
+    );
+  });
+});
+
+color.addEventListener("change", (e) => {
+  boxes.forEach((box) => {
+    ["primary", "success", "attention", "danger", "info"].forEach((i) =>
+      box.classList.remove(i)
+    );
+    e.target.options[e.target.options.selectedIndex].value &&
+      box.classList.add(
+        `${e.target.options[e.target.options.selectedIndex].value}`
+      );
+  });
+});
