a2183adf90d4dbcfe132d47cb7b34135007ada05
phl3bas <phl3bas87@gmail.com>
2020-12-18 16:11:22 +0000

    feature - added radius'

---
+++ packages/core/lib/radius.ts
@@ -0,0 +1,12 @@
+import { TealValueObject } from "../types";
+
+export const Radius: TealValueObject = {
+  radius: [
+    "var(--tl-size-2)",
+    "var(--tl-size-4)",
+    "var(--tl-size-6)",
+    "var(--tl-size-8)",
+    "var(--tl-size-10)",
+    "100%",
+  ],
+};
