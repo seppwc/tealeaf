c0864913bc75fd2f98f2cb05c9ab05eea7106d3d
phl3bas <phl3bas87@gmail.com>
2020-12-18 16:11:42 +0000

    feature: update box shadows

--- packages/core/lib/elevation.ts
+++ packages/core/lib/elevation.ts
@@ -2,14 +2,19 @@ import { TealValueObject } from "../types";
 
 export const Elevation: TealValueObject = {
   shadow: [
-    "0px 2px 4px -1px rgba(0, 0, 0, 0.05), 0px 1px 2px rgba(0, 0, 0, 0.3)",
-    "0px 4px 8px rgba(0, 0, 0, 0.05), 0px 3px 6px rgba(0, 0, 0, 0.1), 0px 2px 4px -1px rgba(0, 0, 0, 0.3)",
-    "0px 9px 17px rgba(0, 0, 0, 0.1), 0px 3px 7px -1px rgba(0, 0, 0, 0.3)",
-    "0px 12px 20px -1px rgba(0, 0, 0, 0.05), 0px 6px 10px rgba(0, 0, 0, 0.1), 0px 4px 10px -2px rgba(0, 0, 0, 0.2)",
-    "0px 12px 24px 3px rgba(0, 0, 0, 0.05), 0px 8px 16px -5px rgba(0, 0, 0, 0.05), 0px 6px 12px -1px rgba(0, 0, 0, 0.15)",
-    "0px 5px 16px -11px rgba(0, 0, 0, 0.05), 0px 2px 7px rgba(0, 0, 0, 0.04), 0px 12px 24px -2px rgba(0, 0, 0, 0.11)",
+    "0px 1px 2px 0px rgba(0, 0, 0, 0.05)",
+    "0px 6px 24px 0px rgba(0, 0, 0, 0.05),0px 0px 0px 1px rgba(0, 0, 0, 0.08)",
+    "0px 2px 5px -2px rgba(50, 50, 93, 0.25),0px 2px 3px -1px rgba(0, 0, 0, 0.3)",
+    "0px 3px 6px -1px rgba(0, 0, 0, 0.05),0px 3px 5px -1px rgba(0, 0, 0, 0.3)",
+    "0px 6px 12px -2px rgba(50, 50, 93, 0.25),0px 3px 7px -3px rgba(0, 0, 0, 0.3)",
+    "0px 13px 27px -5px rgba(50, 50, 93, 0.25),0px 8px 16px -8px rgba(0, 0, 0, 0.3)",
+    "0px 25px 50px -12px rgba(0, 0, 0, 0.25), 0px 10px 24px -8px rgba(0, 0, 0, 0.22)",
+    "0px 20px 30px rgba(0, 0, 0, 0.2)",
   ],
   insetshadow: [
-    "inset 0px 2px 4px rgba(0, 0, 0, 0.16), inset 0px 1px 2px rgba(0, 0, 0, 0.3)",
+    "inset 0px 1px 3px 0px rgba(0, 0, 0, 0.06)",
+    "inset 1px 1px 3px 0px rgb(204, 219, 232),inset -1px -1px 3px 1px rgba(255, 255, 255, 0.5)",
+    "inset 2px 2px 4.5px 0px rgb(204, 219, 232),inset -2px -2px 4.5px 1px rgba(255, 255, 255, 0.5)",
+    "inset 3px 3px 6px 0px rgb(204, 219, 232),inset -3px -3px 6px 1px rgba(255, 255, 255, 0.5)",
   ],
 };
