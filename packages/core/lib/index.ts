70a0a5e79f78abfada8173b99a33515fc83b4aa1
phl3bas <phl3bas87@gmail.com>
2020-12-23 15:03:23 +0000

    feature: change reexport

--- packages/core/lib/index.ts
+++ packages/core/lib/index.ts
@@ -1,5 +1,19 @@
-export { Colors } from "./colors";
-export { Elevation } from "./elevation";
-export { Scale } from "./scale";
-export { Eases } from "./eases";
-export { Radius } from "./radius";
+import { Colors as colors } from "./colors";
+import { Elevation as elevations } from "./elevation";
+import { Scale as scales } from "./scale";
+import { Eases as eases } from "./eases";
+import { Radius as radius } from "./radius";
+
+export const Colors = colors;
+export const Elevation = elevations;
+export const Scale = scales;
+export const Eases = eases;
+export const Radius = radius;
+
+export const CoreProperties = {
+  ...colors,
+  ...elevations,
+  ...scales,
+  ...eases,
+  ...radius,
+};
