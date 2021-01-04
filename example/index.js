d75f2110cc9bda79e7c5b121767e6857961e813c
Sepp Wikramaratna-Clarke <45594209+Phl3bas@users.noreply.github.com>
2021-01-04 17:05:09 +0000

    0002 form styling (#7)
    * chore: installed npm run all and change scripts
    
    * feature: inclue input type button in button styles
    
    * feature: include basic form stylings
    
    * feature: latest build
    
    * docs: update to example page

--- example/index.js
+++ example/index.js
@@ -1,43 +1,16 @@
-// const boxes = document.querySelectorAll(".box");
+const styleSheet = document.querySelector("#stylesheet");
+const toggle = document.querySelector("#toggle");
+const add = document.querySelector("#add");
+const remove = document.querySelector("#remove");
 
-// boxes.forEach((box) =>
-//   box.addEventListener("transitionend", (e) => {
-//     e.target.classList.remove("move");
-//   })
-// );
+toggle.addEventListener("click", () => {
+  if (styleSheet.hasAttribute("disabled")) {
+    add.style.display = "none";
+    remove.style.display = "initial";
+  } else {
+    add.style.display = "initial";
+    remove.style.display = "none";
+  }
 
-// document.querySelector("button").addEventListener("click", () => {
-//   boxes.forEach((box) => {
-//     box.classList.add("move");
-//   });
-// });
-
-// eases.addEventListener("change", (e) => {
-//   boxes.forEach((box) => {
-//     ["in", "out", "inout"].forEach((i) => box.classList.remove(i));
-//     box.classList.add(e.target.options[e.target.options.selectedIndex].value);
-//   });
-// });
-
-// radius.addEventListener("change", (e) => {
-//   boxes.forEach((box) => {
-//     ["r-1", "r-2", "r-3", "r-4", "r-5", "r-full"].forEach((i) =>
-//       box.classList.remove(i)
-//     );
-//     box.classList.add(
-//       `r-${e.target.options[e.target.options.selectedIndex].value}`
-//     );
-//   });
-// });
-
-// color.addEventListener("change", (e) => {
-//   boxes.forEach((box) => {
-//     ["primary", "success", "attention", "danger", "info"].forEach((i) =>
-//       box.classList.remove(i)
-//     );
-//     e.target.options[e.target.options.selectedIndex].value &&
-//       box.classList.add(
-//         `${e.target.options[e.target.options.selectedIndex].value}`
-//       );
-//   });
-// });
+  styleSheet.toggleAttribute("disabled");
+});
