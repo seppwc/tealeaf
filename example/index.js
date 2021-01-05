45c8fd1d12f2528b88c0fe564ca21ceab1df4e12
phl3bas <phl3bas87@gmail.com>
2021-01-05 15:52:12 +0000

    docs: update doc site to be responsive

--- example/index.js
+++ example/index.js
@@ -14,3 +14,24 @@ toggle.addEventListener("click", () => {
 
   styleSheet.toggleAttribute("disabled");
 });
+
+const menubutton = document.querySelector("#menu_button");
+const closebutton = document.querySelector("#close");
+const header = document.querySelector(".header");
+const navLinks = document.querySelectorAll("header nav > a");
+
+console.log(navLinks);
+
+menubutton.addEventListener("click", () => {
+  header.classList.add("open");
+});
+
+closebutton.addEventListener("click", () => {
+  header.classList.remove("open");
+});
+
+navLinks.forEach((link) =>
+  link.addEventListener("click", () => {
+    header.classList.remove("open");
+  })
+);
