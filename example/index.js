baddb1161e194c6798678459a83a0371b23d6610
phl3bas <phl3bas87@gmail.com>
2021-01-06 16:25:57 +0000

    update docs

--- example/index.js
+++ example/index.js
@@ -1,26 +1,7 @@
-const styleSheet = document.querySelector("#stylesheet");
-const toggle = document.querySelector("#toggle");
-const add = document.querySelector("#add");
-const remove = document.querySelector("#remove");
-
-toggle.addEventListener("click", () => {
-  if (styleSheet.hasAttribute("disabled")) {
-    add.style.display = "none";
-    remove.style.display = "initial";
-  } else {
-    add.style.display = "initial";
-    remove.style.display = "none";
-  }
-
-  styleSheet.toggleAttribute("disabled");
-});
-
 const menubutton = document.querySelector("#menu_button");
 const closebutton = document.querySelector("#close");
 const header = document.querySelector(".header");
 const navLinks = document.querySelectorAll("header nav > a");
-
-console.log(navLinks);
 
 menubutton.addEventListener("click", () => {
   header.classList.add("open");
