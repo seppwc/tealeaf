c2dca7a434a0bba710376b9f7c25d7b48d3230ae
phl3bas <phl3bas87@gmail.com>
2021-02-08 13:26:12 +0000

    feature: abstract resolveBody to function

---
+++ packages/css/lib/resolveBody.ts
@@ -0,0 +1,9 @@
+import { convertToKababCase } from "../utils";
+import { isCustomPropertyValue } from "../utils/isCustomPropertyValue";
+import { resolveCustomBody } from "./resolveCustombody";
+
+export function resolveBody(property: string, value: string) {
+  return isCustomPropertyValue(property)
+    ? resolveCustomBody(property, value)
+    : `${convertToKababCase(property)}:${value}`;
+}
