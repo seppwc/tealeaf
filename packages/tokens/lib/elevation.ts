ebfbc43cb926e6fac2b40edd853e6cb4b5c202a2
phl3bas <phl3bas87@gmail.com>
2021-02-15 11:28:27 +0000

    fix: update element values

--- packages/tokens/lib/elevation.ts
+++ packages/tokens/lib/elevation.ts
@@ -5,18 +5,18 @@ export const Elevation: TealValueObject = {
     "0": "none",
     "1": "0px 0px 1px 1px rgba(0, 0, 0, 0.1)",
     "2":
-      "0px 6px 24px 0px rgba(0, 0, 0, 0.05),0px 0px 0px 1px rgba(0, 0, 0, 0.08)",
+      "0px 2px 4px -1px rgba(50, 50, 93, 0.1), 0px 2px 3px -1px rgba(0, 0, 0, 0.18)",
     "3":
-      "0px 2px 5px -2px rgba(50, 50, 93, 0.25),0px 2px 3px -1px rgba(0, 0, 0, 0.3)",
+      "0px 4px 7px -2px rgba(50, 50, 93, 0.18),0px 4px 7px -2px rgba(0, 0, 0, 0.22)",
     "4":
-      "0px 3px 6px -1px rgba(0, 0, 0, 0.05),0px 3px 5px -1px rgba(0, 0, 0, 0.3)",
+      "0px 6px 10px -2px rgba(0, 0, 0, 0.1),0px 6px 10px -2px rgba(0, 0, 0, 0.22)",
     "5":
-      "0px 6px 12px -2px rgba(50, 50, 93, 0.25),0px 3px 7px -3px rgba(0, 0, 0, 0.3)",
+      "0px 8px 12px -2px rgba(50, 50, 93, 0.18),0px 6px 12px -5px rgba(0, 0, 0, 0.22)",
     "6":
-      "0px 13px 27px -5px rgba(50, 50, 93, 0.25),0px 8px 16px -8px rgba(0, 0, 0, 0.3)",
+      "0px 13px 27px -5px rgba(50, 50, 93, 0.22),0px 8px 16px -8px rgba(0, 0, 0, 0.3)",
     "7":
-      "0px 25px 50px -12px rgba(0, 0, 0, 0.25), 0px 10px 24px -8px rgba(0, 0, 0, 0.22)",
-    "8": "0px 20px 30px rgba(0, 0, 0, 0.2)",
+      "0px 25px 50px -12px rgba(0, 0, 0, 0.22), 0px 10px 24px -8px rgba(0, 0, 0, 0.22)",
+    "8": "0px 20px 40px -5px rgba(0, 0, 0, 0.22)",
   },
   insetshadow: {
     "1": "inset 0px 1px 3px 0px rgba(0, 0, 0, 0.1)",
@@ -28,3 +28,5 @@ export const Elevation: TealValueObject = {
       "inset 3px 3px 6px 0px rgb(204, 219, 232),inset -3px -3px 6px 1px rgba(255, 255, 255, 0.5)",
   },
 };
+("");
+("0px 2px 5px -1px rgba(50, 50, 93, 0.25), 0px 1px 3px -1px rgba(0, 0, 0, 0.3);");
