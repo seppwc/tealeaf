3ea2dd8e56aae21bda18ed58eb777e57a8950572
phl3bas <phl3bas87@gmail.com>
2021-02-25 14:47:30 +0000

    css & elements: specificy bug hotfix

--- packages/css/lib/resolveBody.ts
+++ packages/css/lib/resolveBody.ts
@@ -5,5 +5,5 @@ import { resolveCustomBody } from "./resolveCustombody";
 export function resolveBody(property: string, value: string) {
   return isCustomPropertyValue(property)
     ? resolveCustomBody(property, value)
-    : `${convertToKababCase(property)}:${value}`;
+    : `${convertToKababCase(property)}:${value}!important`;
 }
