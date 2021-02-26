27a42cf710f85f5287ab53378e21c870faffc9fd
Sepp Wikramaratna-Clarke <45594209+Phl3bas@users.noreply.github.com>
2021-02-26 14:03:33 +0000

    New scale (#42)
    * tokens - new scale implemented
    
    * elements - update sizes
    
    * tokens size build
    
    * tokens - radius new sizes
    
    * tokens - latest build - sizes

--- packages/tokens/lib/radius.ts
+++ packages/tokens/lib/radius.ts
@@ -3,13 +3,13 @@ import { TealValueObject } from "../types";
 export const Radius: TealValueObject = {
   radius: {
     none: "0",
-    xs: "0.13rem",
+    xs: "0.125rem",
     sm: "0.25rem",
-    md: "0.38rem",
-    lg: "0.68rem",
+    md: "0.375rem",
+    lg: "0.625rem",
     xl: "1rem",
     "2xl": "2rem",
-    "3xl": "3.87rem",
+    "3xl": "3.75rem",
     round: "9999px",
   },
 };
